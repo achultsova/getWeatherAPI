ab817b3b2157ea2188441249f494c7c7daee6fad
ICX\anastasiya.chultsova <nchulczova@gmail.com>
2021-10-21 18:31:14 +0300

    feat: add input and output to the screen

--- weather.js
+++ weather.js
@@ -1,15 +1,37 @@
-const url = 'https://api.m3o.com/v1/weather/Forecast';
-fetch (url + `?days=2&location=London`, {
-    method: "GET",
-    headers: {
-        'Content-Type': 'application/json',
-        "Authorization": "Bearer NWZlYzNjNTEtZjYyOC00YzZjLTk4MzYtYTk3ZTlkNWVkMDA1"
-      }
-    })
-    .then (resp => resp.json())
-    .then (function(data){
-        console.log(data);
-    })
-    .catch (function(error) {
-        console.log(error);
-    })
+const inputDays = document.querySelector('.days'),
+inputCity = document.querySelector('.city'),
+btnRun = document.querySelector('.btn'),
+outputted = document.querySelector('.outputted');
+
+
+    inputDays.addEventListener('change', () => {
+        localStorage.setItem("inputDays", inputDays.value)
+    });
+
+    inputCity.addEventListener('change', () => {
+        localStorage.setItem("inputCity", inputCity.value)
+    });
+
+    btnRun.addEventListener('click', () => {
+
+        const days = localStorage.getItem('inputDays');
+        const city = localStorage.getItem('inputCity');
+
+
+        const url = 'https://api.m3o.com/v1/weather/Forecast';
+        fetch (url + `?days=${days}&location=${city}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": "Bearer NWZlYzNjNTEtZjYyOC00YzZjLTk4MzYtYTk3ZTlkNWVkMDA1"
+        }
+        })
+        .then (resp => resp.json())
+        .then (JSON.stringify)
+        .then (function(data){
+            outputted.innerHTML = data ;
+        })
+        .catch (function(error) {
+            outputted.innerHTML = error;
+        })
+        });
